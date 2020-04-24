d3beded1b2429bc140c39f0b21979f173c746be8
Suryansh Tripathi <Suryansh1004@gmail.com>
2020-04-24 17:44:10 +0530

    Add files via upload

---
+++ fshare-master/lib/main.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttershare/pages/home.dart';
+
+void main() {
+  FlutterError.onError = (FlutterErrorDetails details) {
+    FlutterError.dumpErrorToConsole(details);
+    if (kReleaseMode)
+      exit(1);
+  };
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FlutterShare',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        accentColor: Colors.teal,
+      ),
+      home: Home(),
+    );
+  }
+}
---
+++ fshare-master/lib/widgets/header.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+AppBar header(context, {bool isAppTitle = false, String titleText, removeBackButton = false}) {
+  return AppBar(
+    automaticallyImplyLeading: removeBackButton ? false : true,
+    title: Text(
+      isAppTitle ? "FShare" : titleText,
+      style: TextStyle(
+        color: Colors.white,
+        fontFamily: isAppTitle ? "Signatra" : "aa",
+        fontSize: isAppTitle ? 50.0 : 22.0,
+      ),
+      overflow: TextOverflow.ellipsis,
+    ),
+    centerTitle: true,
+    backgroundColor: Theme.of(context).accentColor,
+  );
+}
